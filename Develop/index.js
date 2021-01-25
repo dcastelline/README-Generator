eaf8969fba4d09c09b3d06b5e57ed91124410af5
Daniel <daniel.castelline@gmail.com>
2021-01-24 21:48:12 -0600

    changed license list in index.js, added all license info to generateMarkdown.js, placed data.year and data.firstLast in proper spots in licenses for copywrite, added if else statements for license selections, return info

--- Develop/index.js
+++ Develop/index.js
@@ -51,8 +51,8 @@ const questions = [
         name: 'license',
         type: 'list',
         message: 'Choose a license for your project.',
-        choices: ['Academic Free License v3.0', 'Apache license 2.0', 'Artistic license 2.0', 'Boost Software License 1.0', 'BSD 2-clause "Simplified" license', 'BSD 3-clause "New" or "Revised" license', 'BSD 3-clause Clear license', 'Creative Commons license family', 'Creative Commons Zero v1.0 Universal', 'Creative Commons Attribution 4.0', 'Creative Commons Attribution Share Alike 4.0', 'Do What The F*ck You Want To Public License', 'Educational Community License v2.0', 'Eclipse Public License 1.0', 'Eclipse Public License 2.0', 'European Union Public License 1.1', 'GNU Affero General Public License v3.0', 'GNU General Public License family', 'GNU General Public License v2.0', 'GNU General Public License v3.0', 'GNU Lesser General Public License family', 'GNU Lesser General Public License v2.1', 'GNU Lesser General Public License v3.0', 'ISC', 'LaTeX Project Public License v1.3c', 'Microsoft Public License', 'MIT', 'Mozilla Public License 2.0', 'Open Software License 3.0', 'PostgreSQL License', 'SIL Open Font License 1.1', 'University of Illinois/NCSA Open Source License', 'The Unlicense', 'zLib License'],
-     },
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'], 
+    },
      {
         name: 'contributorConfirm',
         type: 'confirm',
@@ -82,6 +82,16 @@ const questions = [
          name: 'email',
          type: 'input',
          message: 'Enter your email address.',
+     },
+     {
+         name: 'year',
+         type: 'input',
+         message: 'Enter the current year "YYYY".',
+     },
+     {
+         name: 'firstLast',
+         type: 'input',
+         message: 'Enter your first and last name.',
      },
 ];
 
