81027ae33dcb0d71683067e9ab199545c084256a
Daniel <daniel.castelline@gmail.com>
2021-01-25 19:32:15 -0600

    committing working final product

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2150,33 +2150,51 @@ function generateMarkdown(data) {
     
     For more information, please refer to <https://unlicense.org>`;
 
+  let badge = '';
+  const gnuAgplv3Badge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+  const gnuGplv3Badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  const gnuLgplv3Badge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+  const mozPubLic2Badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+  const apacheLic2Badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  const mitBage = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  const boostSoftLic1Badge = '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+  const theUnlicenseBadge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+
   if (data.license === 'GNU AGPLv3') {
     userLicense = gnuAgplv3;
+    badge = gnuAgplv3Badge;
   }
   else if (data.license === 'GNU GPLv3') {
     userLicense = gnuGplv3;
+    badge = gnuGplv3Badge;
   }
   else if (data.license === 'GNU LGPLv3') {
     userLicense = gnuLgplv3;  
+    badge = gnuLgplv3Badge;
   }
   else if (data.license === 'Mozilla Public License 2.0') {
     userLicense = mozPubLic2;  
+    badge = mozPubLic2Badge;
   }
   else if (data.license === 'Apache License 2.0') {
-    userLicense = apacheLic2;  
+    userLicense = apacheLic2; 
+    badge = apacheLic2Badge;
   }
   else if (data.license === 'MIT License') {
     userLicense = mit;  
+    badge = mitBage;
   }
   else if (data.license === 'Boost Software License 1.0') {
     userLicense = boostSoftLic1;  
+    badge = boostSoftLic1Badge;
   }
   else {
     userLicense = theUnlicense;  
+    badge = theUnlicenseBadge;
   }
 
   return `# ${data.title}
-  ![Badge](https://img.shields.io/badge/license-${data.license}-green)<br>
+  ${badge}
   ## Desctiption
     ${data.description}
 
--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# README-Generator
+# README Generator
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  ## Desctiption
+    This project was completed using Node.js and inquirer npm to automatically generate a README in markdown from answers to prompts.
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  Install Node.js
+
+  ## Usage
+  In Visual Studio Code, in the Integrated Terminal, type the command 'node index.js' to begin the program.  Then answer the questions you are prompted with.  After answering questions, a README.md file will be created and you can make more adjustments to it.
+
+  ## License
+  MIT License
+
+    Copyright (c) 2021 Daniel Castelline
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+  ## Contributing
+  N/A
+
+  ## Tests
+  N/A
+
+  ## Questions
+  You can contact me via GitHub or email with questions.
+  GitHub: https://www.github.com/dcastelline<br>
+  Email: daniel.castelline@gmail.com
+  
